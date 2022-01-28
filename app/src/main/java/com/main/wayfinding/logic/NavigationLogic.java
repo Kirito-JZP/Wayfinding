097cb112800dbfade2b6f6debdc67c9dcd35e4e9
JIA <jiazh@tcd.ie>
2022-01-28 17:19:36 +0000

    Add navigation logic

---
+++ app/src/main/java/com/main/wayfinding/logic/NavigationLogic.java
@@ -0,0 +1,12 @@
+package com.main.wayfinding.logic;
+
+/**
+ * Logic of Navigation
+ *
+ * @author jia72
+ * @author Last Modified By jia72
+ * @version Revision: 0
+ * Date: 2022/1/28 17:18
+ */
+public class NavigationLogic {
+}
