d94d9fa8b6399061616cf6a3dc81ce4ccd47f650
Gang An <15666671996@163.com>
2022-01-28 17:01:57 +0000

    working 01281701

--- app/src/main/java/com/main/wayfinding/fragment/account/AccountFragment.java
+++ app/src/main/java/com/main/wayfinding/fragment/account/AccountFragment.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-//import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.main.wayfinding.R;
 import com.main.wayfinding.databinding.FragmentAccountBinding;
 
 
@@ -21,26 +27,61 @@ import com.main.wayfinding.databinding.FragmentAccountBinding;
  */
 public class AccountFragment extends Fragment {
     private FragmentAccountBinding binding;
-//    private FirebaseAuth auth;
+    private FirebaseAuth auth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         return root;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+    }
 
-//        auth = FirebaseAuth.getInstance();
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            TextView status = getView().findViewById(R.id.status);
+            status.setText("Y");
+        } else {
+            TextView status = getView().findViewById(R.id.status);
+            status.setText("N");
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText username = getView().findViewById(R.id.username);
+                System.out.println(username.getText().toString());
+            }
+        });
+        view.findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText username = getView().findViewById(R.id.username);
+                EditText password = getView().findViewById(R.id.password);
+
+            }
+        });
+
+
     }
 
 
