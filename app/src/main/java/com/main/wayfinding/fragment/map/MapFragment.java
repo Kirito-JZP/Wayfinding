6e7eb64ba21c8ee311cd099a62106d5d9bb5d532
Zehua Guo <zguo@tcd.ie>
2022-01-29 00:24:49 +0000

    modified MapFragment.java

--- app/src/main/java/com/main/wayfinding/fragment/map/MapFragment.java
+++ app/src/main/java/com/main/wayfinding/fragment/map/MapFragment.java
@@ -2,9 +2,11 @@ package com.main.wayfinding.fragment.map;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -19,11 +21,17 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.PlacesApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.AutocompletePrediction;
 import com.main.wayfinding.R;
+import com.main.wayfinding.WayfindingApp;
 import com.main.wayfinding.databinding.FragmentMapBinding;
+import com.main.wayfinding.dto.LocationDto;
 import com.main.wayfinding.logic.NavigationLogic;
 import com.main.wayfinding.utility.GPSTracker;
-import com.main.wayfinding.dto.LocationDto;
+
+import java.io.IOException;
 
 /**
  * Define the fragment used for displaying map and dynamic Sustainable way-finding
@@ -87,6 +95,28 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 if (currentLocation != null) {
                     navigation.findRoute(currentLocation, new LatLng(53.3706544, -6.3336711));
                 }
+            }
+        });
+
+        EditText searchBox = (EditText) view.findViewById(R.id.input_search);
+        searchBox.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_UP) {
+                    Location location = gps.getLocation(getActivity());
+                    try {
+                        AutocompletePrediction[] predictions = PlacesApi.queryAutocomplete(WayfindingApp.getGeoApiContext(), searchBox.getText().toString())
+                                .location(new com.google.maps.model.LatLng(location.getLatitude(), location.getLongitude()))
+                                .radius(50000)
+                                .await();
+                        for (AutocompletePrediction prediction : predictions) {
+                            // TODO: UI
+                        }
+                    } catch (ApiException | IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return false;
             }
         });
 
