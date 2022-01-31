2d29bb5bc8472a30432bdd58b53b82673ac8a9c4
Ameya Kale <ameykale603@gmail.com>
2022-01-31 15:43:00 +0000

    Added an image to preference fragment

--- app/src/main/java/com/main/wayfinding/fragment/preference/PreferenceFragment.java
+++ app/src/main/java/com/main/wayfinding/fragment/preference/PreferenceFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,22 +23,14 @@ import com.main.wayfinding.databinding.FragmentPreferenceBinding;
  */
 public class PreferenceFragment extends Fragment {
     private FragmentPreferenceBinding binding;
-
-    private ListView listView;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_preference, container, false);
 
-        listView = (ListView) view.findViewById(R.id.list_view);
-
-
-        String preferences[] = {"Preference1", "Preference2", "Preference3", "Preference4", "Preference5"};
-
-        listView.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(),
-                android.R.layout.preference_category, preferences));
-
-        return view;
+        View rootView =inflater.inflate(R.layout.preference_image, container, false);
+        ImageView imageView=(ImageView) rootView.findViewById(R.id.image);
+        // Inflate the layout for this fragment
+        imageView.setImageResource(R.drawable.cafe);
+        return rootView;
     }
     @Override
     public void onDestroyView() {
