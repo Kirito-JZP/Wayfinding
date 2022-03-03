c8a0966800aa34a5e3ecb5a9551e91d239f18c6a
Sahil Guleria <sahilg1720@gmail.com>
2022-03-03 18:06:59 +0000

    UI changes to preference xm

--- app/src/main/java/com/main/wayfinding/adapter/preferenceAdapter.java
+++ app/src/main/java/com/main/wayfinding/adapter/preferenceAdapter.java
@@ -1,10 +1,16 @@
 package com.main.wayfinding.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,10 +18,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.main.wayfinding.R;
 import com.main.wayfinding.dto.LocationDto;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class preferenceAdapter extends RecyclerView.Adapter<preferenceAdapter.ViewHolder>
 {
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+
+        public ImageView imageImageView;
         public TextView nameTextView;
         public TextView addressTextView;
         public TextView countryTextView;
@@ -23,6 +33,7 @@ public class preferenceAdapter extends RecyclerView.Adapter<preferenceAdapter.Vi
         public ViewHolder(View itemView)
         {
             super(itemView);
+            imageImageView = (ImageView) itemView.findViewById(R.id.image);
             nameTextView = (TextView) itemView.findViewById(R.id.textName);
             addressTextView = (TextView) itemView.findViewById(R.id.textAddress);
             countryTextView = (TextView) itemView.findViewById(R.id.textCountry);
@@ -48,6 +59,25 @@ public class preferenceAdapter extends RecyclerView.Adapter<preferenceAdapter.Vi
     public void onBindViewHolder(preferenceAdapter.ViewHolder holder, int position)
     {
         LocationDto recentlySaved = mRecentlySaved.get(position);
+
+        if (StringUtils.isNotEmpty(recentlySaved.getGmImgUrl())) {
+            new Thread(() -> {
+                try {
+                    // resolving the string into url
+                    URL url = new URL(recentlySaved.getGmImgUrl());
+                    // Open the input stream
+                    InputStream inputStream = url.openStream();
+                    // Convert the online source to bitmap picture
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    ImageView imageView = holder.imageImageView;
+                    imageView.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        } else {
+
+        }
 
         TextView textView = holder.nameTextView;
         textView.setText(recentlySaved.getName());
--- app/src/main/java/com/main/wayfinding/fragment/preference/PreferenceFragment.java
+++ app/src/main/java/com/main/wayfinding/fragment/preference/PreferenceFragment.java
@@ -64,8 +64,6 @@ public class PreferenceFragment extends Fragment {
 
 
 
-
-
         return rootView;
 
     }
