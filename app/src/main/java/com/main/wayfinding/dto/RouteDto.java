5f1304ebd1e5a396368e53a16483ca8e0d98bac0
Zehua Guo <zguo@tcd.ie>
2022-03-31 00:10:40 +0100

    update RouteDto.java

--- app/src/main/java/com/main/wayfinding/dto/RouteDto.java
+++ app/src/main/java/com/main/wayfinding/dto/RouteDto.java
@@ -1,6 +1,12 @@
 package com.main.wayfinding.dto;
 
+import android.graphics.Color;
+import android.util.Pair;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.model.TravelMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +22,68 @@ import java.util.List;
 
 
 public class RouteDto {
+    public static final class RouteStep {
+        private PolylineOptions option;
+        private Polyline line;
+        private LocationDto startLocation;
+        private LocationDto endLocation;
+        private TravelMode mode;
+        private boolean passed;
+
+        public TravelMode getMode() {
+            return mode;
+        }
+
+        public void setMode(TravelMode mode) {
+            this.mode = mode;
+        }
+
+        public boolean isPassed() {
+            return passed;
+        }
+
+        public void setPassed(boolean passed) {
+            this.passed = passed;
+            // change the colour of the polyline
+            // note that until map.addPolyline is called, the settings won't be applied
+            option.color(Color.DKGRAY);
+        }
+
+        public PolylineOptions getOption() {
+            return option;
+        }
+
+        public void setOption(PolylineOptions option) {
+            this.option = option;
+        }
+
+        public Polyline getLine() {
+            return line;
+        }
+
+        public void setLine(Polyline line) {
+            this.line = line;
+        }
+
+        public LocationDto getStartLocation() {
+            return startLocation;
+        }
+
+        public void setStartLocation(LocationDto startLocation) {
+            this.startLocation = startLocation;
+        }
+
+        public LocationDto getEndLocation() {
+            return endLocation;
+        }
+
+        public void setEndLocation(LocationDto endLocation) {
+            this.endLocation = endLocation;
+        }
+    }
+
     public RouteDto() {
-        polylineOptionsList = new ArrayList<>();
+        steps = new ArrayList<>();
         waypoints = new ArrayList<>();
     }
 
@@ -27,6 +93,10 @@ public class RouteDto {
 
     public void setStartLocation(LocationDto startLocation) {
         this.startLocation = startLocation;
+        // calculate forward direction unit if possible
+//        if (endLocation != null) {
+//            calculateForwardDirection();
+//        }
     }
 
     public LocationDto getEndLocation() {
@@ -35,6 +105,10 @@ public class RouteDto {
 
     public void setEndLocation(LocationDto endLocation) {
         this.endLocation = endLocation;
+        // calculate forward direction unit if possible
+//        if (startLocation != null) {
+//            calculateForwardDirection();
+//        }
     }
 
     public List<LocationDto> getWaypoints() {
@@ -45,17 +119,97 @@ public class RouteDto {
         waypoints.add(waypoint);
     }
 
-    public void addPolylineOptions(PolylineOptions options) {
-        polylineOptionsList.add(options);
+    public List<RouteStep> getSteps() {
+        return steps;
     }
 
-    public List<PolylineOptions> getAllPolylineOptions() {
-        return polylineOptionsList;
+    public void addStep(RouteStep step) {
+        steps.add(step);
+    }
+
+    public TravelMode getMode() {
+        return mode;
+    }
+
+    public void setMode(TravelMode mode) {
+        this.mode = mode;
+    }
+
+    public int getCurrentStepIndex() {
+        return currentStepIndex;
+    }
+
+    public void updateUI(GoogleMap map) {
+        for (RouteDto.RouteStep step : getSteps()) {
+            Polyline line = map.addPolyline(step.getOption());
+            step.setLine(line);
+        }
+    }
+
+    public void updateRouteState(LocationDto currentLocation) {
+        boolean hasFoundCurrentStep = false;
+        int index = 0;
+        for (RouteStep step : steps) {
+            if(step.isPassed()) {
+                continue;
+            }
+            LocationDto startLocation = step.getStartLocation();
+            LocationDto endLocation = step.getEndLocation();
+            Pair<Float, Float> forwardLatLng = calculateForwardDirection(startLocation, endLocation);
+            float forwardLat = forwardLatLng.first;
+            float forwardLng = forwardLatLng.second;
+            // use the symbol of the difference between the "end location" and the "start
+            // location" to
+            // judge whether the step should be marked as "passed"
+            // the "end location" here refers to the start location of a step and the "start
+            // location"
+            // refers to the parameter currentLocation
+            // if the symbol matches the precalculated forwardLng/forwardLat, then the
+            // latitude/longitude
+            // must be towards the end location of the whole route
+            if ((startLocation.getLongitude() - currentLocation.getLongitude()) * forwardLng >= 0 ||
+                    (startLocation.getLatitude() - currentLocation.getLatitude()) * forwardLat >= 0) {
+                if (!hasFoundCurrentStep) {
+                    currentStepIndex = index;
+                    hasFoundCurrentStep = true;
+                }
+                step.setPassed(false);
+            } else {
+                step.setPassed(true);
+            }
+            index += 1;
+        }
+    }
+
+    public boolean replaceOrAddStep(int index, RouteStep step) {
+        try {
+            if (index < steps.size()) {
+                steps.set(index, step);
+            } else {
+                steps.add(index, step);
+            }
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    public boolean judgePassed(LocationDto location, LocationDto reference, float forwardLat, float forwardLng) {
+        return false;
+    }
+
+    private Pair<Float, Float> calculateForwardDirection(LocationDto startLocation, LocationDto endLocation) {
+        float forwardLat = endLocation.getLatitude() - startLocation.getLatitude() > 0 ?
+                1.0F : -1.0F;
+        float forwardLng = endLocation.getLongitude() - startLocation.getLongitude() > 0
+                ? 1.0F : -1.0F;
+        return new Pair<Float, Float>(forwardLat, forwardLng);
     }
 
     private LocationDto startLocation;
     private LocationDto endLocation;
-    private List<LocationDto> waypoints;
-    private List<PolylineOptions> polylineOptionsList;
-
+    private TravelMode mode;
+    private int currentStepIndex;
+    private final List<RouteStep> steps;
+    private final List<LocationDto> waypoints;
 }
